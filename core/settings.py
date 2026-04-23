477f4cf023308f68938d42eb60eefbb6168fbf3e
KingBen-j <coulibalyisrael39@gmail.com>
2026-04-23 02:28:43 +0000

    error versel commit

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-change-me"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'mon-portfolio-ten-iota.vercel.app',
+    'localhost',
+    '127.0.0.1',
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
